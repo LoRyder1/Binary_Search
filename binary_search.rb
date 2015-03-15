db27bae1adbc72a647b438c8f76dcc7ea7df59c8
Lovinder Pnag <lspnag@gmail.com>
2015-03-15 11:56:03 -0500

    working on understanding binary search

---
+++ binary_search.rb
@@ -0,0 +1,60 @@
+require 'benchmark'
+
+def linear_search diamonds, my_diamond
+  comparisons = 0
+  diamonds.each do |diamond|
+    if diamond == my_diamond
+      puts "Found it!"
+      return true 
+    else
+      comparisons += 1
+    end
+    puts "Comparison #{comparisons}, diamond of diameter #{diamond}"
+  end
+  puts "Homey! your diamond ain't here"
+  return false
+end
+
+
+def binary_search diamonds, my_diamond
+  sorted_diamonds = diamonds.sort
+  lower_bound   = 0
+  upper_bound   = diamonds.size
+  comparisons   = 0
+
+  while lower_bound <= upper_bound
+    mid_point = ((lower_bound + upper_bound) / 2).floor
+    # p diamonds.slice(lower_bound..upper_bound)
+    if my_diamond < sorted_diamonds[mid_point]
+      upper_bound = mid_point - 1
+      comparisons += 1
+      puts
+    elsif my_diamond > sorted_diamonds[mid_point]
+      lower_bound = mid_point + 1
+      comparisons += 1
+      puts
+    else
+      puts "Found it!"
+      return true
+    end
+    puts "Comparison #{comparisons}"
+  end
+  puts "Homey! your diamond ain't here"
+  return false
+end
+
+diamonds = [] #my bowl of diamonds
+
+# 10000000.times do 
+1_000_000.times do 
+  diamonds << rand(1..1_000_000) #make an diamond with a random diameter
+end
+diamonds << 1_000_001
+
+my_diamond = 1_000_001 #set my_diamond diameter to compare to
+
+puts "Linear Search"
+puts Benchmark.measure { linear_search(diamonds, my_diamond) }
+puts "Binary Search"
+puts Benchmark.measure { binary_search(diamonds, my_diamond) }
+
